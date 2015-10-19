4a229fe22cf1a43145b65804a8bd497d0192d382
upbeatvision <info@upbeatvision.com>
2015-10-19 04:56:20 +0000

    image02

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,5 +2,5 @@ class Micropost < ActiveRecord::Base
   belongs_to :user
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
-  mount_uploader :image, ImageUploader
+  mount_uploader :image, ImageUploader #carrierwave
 end
